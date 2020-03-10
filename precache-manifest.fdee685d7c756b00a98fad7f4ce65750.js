7ca9ed62e0d0fa6e836a844b0ba2960aea2b8b6c
Sfundo Mhlungu <mhlungusk@gmail.com>
2020-03-10 21:38:48 +0200

    Markdown

---
+++ precache-manifest.fdee685d7c756b00a98fad7f4ce65750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec0d3f91f97a1184069c6f797d5fcf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4ec135046d07e6a779b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba5935e28ffe62ae4708",
+    "url": "/static/js/2.1ac3ff71.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1ac3ff71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ec135046d07e6a779b",
+    "url": "/static/js/main.60c755b8.chunk.js"
+  },
+  {
+    "revision": "2c8039270614a39c8cea",
+    "url": "/static/js/runtime-main.44716643.js"
+  }
+]);
